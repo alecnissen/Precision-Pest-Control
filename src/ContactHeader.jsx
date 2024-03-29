2e61715b1da6a212d30abe435df0f3a165fe7661
alecnissen <alecnissen@yahoo.com>
2024-03-28 22:53:03 -0400

    added content down for contact header, now will try to put the svg icon next to email and phone

--- src/ContactHeader.jsx
+++ src/ContactHeader.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function ContactHeader() {
   return (
     <div className='contact-header-content-wrapper'>
-      <h1>Hello World</h1>
+
+      <div className='contact-header-icons-container'>
+      <img src="../assets/icons8-facebook (1).svg"></img>
+      <img src="../assets/instagram-svgrepo-com (1).svg"></img>
+      </div>
+
+      <div className='contact-header-phone-email-container'>
+        <p>matt@prodigypestsolutions.com</p>
+        <p>267-603-1811</p>
+      </div>
+
     </div>
   )
 }

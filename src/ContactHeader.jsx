9bc2368afdd9449d9d41a82319c41cc5ef5fabe5
alecnissen <alecnissen@yahoo.com>
2024-03-31 19:40:58 -0400

    saving work, going to begin to implement new design pattern

--- src/ContactHeader.jsx
+++ src/ContactHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ContactHeader() {
   return (
     <div className="contact-header-content-wrapper">
-      <div className="contact-header-icons-container">
+      {/* <div className="contact-header-icons-container">
         <img src="../assets/icons8-facebook (1).svg"></img>
         <img src="../assets/instagram-svgrepo-com (1).svg"></img>
       </div>
@@ -18,7 +18,14 @@ export default function ContactHeader() {
           <img src="../assets/telephone-svgrepo-com.svg"></img>
           <p>267-603-1811</p>
         </div>
-      </div>
+      </div> */}
+
+      <h1>Prodigy Pest Solutions</h1>
+      <h3>We Got You Covered Philly!</h3>
+
+
+
+
     </div>
   );
 }
--- src/ImageTitleComponent.jsx
+++ src/ImageTitleComponent.jsx
@@ -5,13 +5,16 @@ export default function ImageTitleComponent() {
   return (
     <div className='image-title-content-wrapper'>
 
-        <div className='main-title-img-container'> 
 
+        <h1>Proudly Serving The Philadelphia Area!</h1>
+
+        <div className='main-title-img-container'> 
+{/* 
             <div className='main-title-container'>
             <h1>Prodigy Pest Solutions</h1>
             <h3>We Got You Covered Philly!</h3>
             <h3>Proudly Serving the PA, NJ, DE areas!</h3>
-            </div>
+            </div> */}
 
             {/* <img src={ppsLogo}></img> */}
 

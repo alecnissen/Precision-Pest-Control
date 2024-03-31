bbe9552564b76b98ed1426599ea6f95df5e130be
alecnissen <alecnissen@yahoo.com>
2024-03-30 20:11:12 -0400

    added media queries for contact header, and also the navbar, made the navbar and contact header responsive and also made sure the dropdowns fit within smaller screens

--- src/ContactHeader.jsx
+++ src/ContactHeader.jsx
@@ -1,29 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function ContactHeader() {
   return (
-    <div className='contact-header-content-wrapper'>
-
-      <div className='contact-header-icons-container'>
-      <img src="../assets/icons8-facebook (1).svg"></img>
-      <img src="../assets/instagram-svgrepo-com (1).svg"></img>
+    <div className="contact-header-content-wrapper">
+      <div className="contact-header-icons-container">
+        <img src="../assets/icons8-facebook (1).svg"></img>
+        <img src="../assets/instagram-svgrepo-com (1).svg"></img>
       </div>
 
-      <div className='contact-header-phone-email-container'>
-        
-        <div className='email-container'>
-        <img src="../assets/email-1-svgrepo-com.svg"></img>
-        <p>matt@prodigypestsolutions.com</p>
+      <div className="contact-header-phone-email-container">
+        <div className="email-container">
+          <img src="../assets/email-1-svgrepo-com.svg"></img>
+          <p>matt@prodigypestsolutions.com</p>
         </div>
 
-        <div className='phone-container'>
-        <img src='../assets/telephone-svgrepo-com.svg'></img>
-        <p>267-603-1811</p>
+        <div className="phone-container">
+          <img src="../assets/telephone-svgrepo-com.svg"></img>
+          <p>267-603-1811</p>
         </div>
-
-
       </div>
-
     </div>
-  )
+  );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,104 +1,83 @@
-import React from 'react'
-import arrowRightIcon from "../assets/arrowright-svgrepo-com.svg"
+import React from 'react';
+import arrowRightIcon from '../assets/arrowright-svgrepo-com.svg';
 
 export default function Navbar() {
   return (
-    <div className='navbar-content-wrapper'>
-
-        <div className='dropdown'>
+    <div className="navbar-content-wrapper">
+      <div className="dropdown">
         <button>Services</button>
 
-        <div className='content'> 
-            <a href=''>Ants</a>
-            <a href=''>Bed Bugs</a>
-            <a href=''>Cockroaches</a>
-            <a href=''>Fleas</a>
-            <a href=''>Mosquito</a>
-            <a href=''>Spiders</a>
-            <a href=''>Stinging Insects</a>
-            <a href=''>Termites</a>
-            <a href=''>Ticks</a>
+        <div className="content">
+          <a href="">Ants</a>
+          <a href="">Bed Bugs</a>
+          <a href="">Cockroaches</a>
+          <a href="">Fleas</a>
+          <a href="">Mosquito</a>
+          <a href="">Spiders</a>
+          <a href="">Stinging Insects</a>
+          <a href="">Termites</a>
+          <a href="">Ticks</a>
         </div>
+      </div>
 
-        </div>
-
-        <div className='dropdown'>
+      <div className="dropdown">
         <button>Service Areas</button>
 
-        <div className='content'> 
-            <a href=''>Delaware County</a>
-            <a href=''>Philadelphia</a>
-            <a href=''>Chester County</a>
-            <a href=''>Montgomery County</a>
-            <a href=''>Bucks County</a>
-         
+        <div className="content">
+          <a href="">Delaware County</a>
+          <a href="">Philadelphia</a>
+          <a href="">Chester County</a>
+          <a href="">Montgomery County</a>
+          <a href="">Bucks County</a>
         </div>
+      </div>
 
-        </div>
-        
-
-        <div className='dropdown'>
+      <div className="dropdown">
         <button>Pest ID</button>
 
-        <div className='content'> 
-            <a href=''>Ants</a>
-            <a href=''>Bed Bugs</a>
-            <a href=''>Boxelder Bugs</a>
-            <a href=''>Carpenter Bees</a>
-            <a href=''>Cockroaches</a>
-            <a href=''>Fleas</a>
-            <a href=''>Mice</a>
-            <a href=''>Mosquitos</a>
-            <a href=''>Rats</a>
-            <a href=''>Spiders</a>
+        <div className="content">
+          <a href="">Ants</a>
+          <a href="">Bed Bugs</a>
+          <a href="">Boxelder Bugs</a>
+          <a href="">Carpenter Bees</a>
+          <a href="">Cockroaches</a>
+          <a href="">Fleas</a>
+          <a href="">Mice</a>
+          <a href="">Mosquitos</a>
+          <a href="">Rats</a>
+          <a href="">Spiders</a>
 
-
-            <div className='dropdown-for-stinging-pests'>  
-
-            <div className='stinging-pests-dropdown-icon-container'>
-            <a href=''>Stinging Pests</a>
-            <img src={arrowRightIcon}></img>
+          <div className="dropdown-for-stinging-pests">
+            <div className="stinging-pests-dropdown-icon-container">
+              <a href="">Stinging Pests</a>
+              <img src={arrowRightIcon}></img>
             </div>
 
-            <div className='content-for-stinging-pests'>
-            <a href=''>Yellow Jackets</a>
-            <a href=''>European Hornets</a>
-            <a href=''>Bald Faced Hornets</a>
+            <div className="content-for-stinging-pests">
+              <a href="">Yellow Jackets</a>
+              <a href="">European Hornets</a>
+              <a href="">Bald Faced Hornets</a>
             </div>
-            </div>
+          </div>
 
+          <a href="">Termites</a>
 
-
-            <a href=''>Termites</a>
-
-            
-            <div className='dropdown-for-other-pests'>  
-
-            <div className='other-pests-dropdown-icon-container'>
-            <a href=''>Other</a>
-            <img src={arrowRightIcon}></img>
+          <div className="dropdown-for-other-pests">
+            <div className="other-pests-dropdown-icon-container">
+              <a href="">Other</a>
+              <img src={arrowRightIcon}></img>
             </div>
 
-
-            <div className='content-for-other-pests'>
-            <a href=''>Stick Bugs</a>
-            <a href=''>Lady Bugs</a>
+            <div className="content-for-other-pests">
+              <a href="">Stick Bugs</a>
+              <a href="">Lady Bugs</a>
             </div>
-            </div>
-
-
-
+          </div>
         </div>
+      </div>
 
-        </div>
-
-
-       
-        <button>About Us</button>
-        <button>Contact</button>
-
-
-
+      <button>About Us</button>
+      <button>Contact</button>
     </div>
-  )
+  );
 }

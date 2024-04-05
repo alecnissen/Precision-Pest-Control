19c31acc1349fcb6b94954855acdbfe9da2d4deb
alecnissen <alecnissen@yahoo.com>
2024-04-04 23:23:13 -0400

    moved the image text to the center as well as applying a max-width to the image text, also added button at the bottom of the description container

--- src/ImageTitleComponent.jsx
+++ src/ImageTitleComponent.jsx
@@ -4,12 +4,13 @@ import ppsLogo from '../assets/PPS-logo.jpeg';
 export default function ImageTitleComponent() {
   return (
     <div className="image-title-content-wrapper">
-      <div className="wrapper">
+      <div className="description-container">
         <p>
           As a Family Owned & Operated company with over 35 years of combined experience, Prodigy Pest Solutions offers
           a quick and reliable solution to your pest concerns. If you are looking for the best bed bug exterminator in
           the Delaware Valley, you’ve come to the right place!
         </p>
+        <button>Contact</button>
       </div>
     </div>
   );

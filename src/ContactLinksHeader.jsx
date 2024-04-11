fcff6d2f4a84f9d748a857c87a75371e8f646ea4
alecnissen <alecnissen@yahoo.com>
2024-04-10 23:53:04 -0400

    added all content for contact header, now will work on making the section responsive

--- src/ContactLinksHeader.jsx
+++ src/ContactLinksHeader.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import phoneIcon from '../assets/telephone-svgrepo-com.svg';
-import instaIcon from '../assets/instagram-svgrepo-com (1).svg';
-import fbIcon from '../assets/icons8-facebook (1).svg';
+import instaIcon from '../assets/instagram-logo-facebook-2-svgrepo-com (1).svg';
+import fbIcon from '../assets/fb-logo.svg';
+import linkedinIcon from '../assets/linkedin-svgrepo-com (1).svg';
 
 
 export default function ContactLinksHeader() {
@@ -10,18 +11,38 @@ export default function ContactLinksHeader() {
         
     <div className='phone-email-hours-container'> 
 
-    
+    <div className='phone-container-contact-header'>
     <p>267-603-1811</p>
+    </div>
 
-
+    <div className='email-container-contact-header'>
     <p>matt@prodigypestsolutions.com</p>
+    </div>
+    
+    <div className='hours-container-contact-header'>
     <p>Mon-Fri: 8:00am - 4:00pm</p>
+    </div>
+
+
+
 
     </div>
 
     <div className='social-media-icons-container'> 
+
+        <div className='insta-icon-container-contact-header'>
         <img src={instaIcon}></img>
+        </div>
+
+        <div className='fb-container-contact-header'>
         <img src={fbIcon}></img>
+        </div>
+
+        <div className='linkedin-container-contact-header'> 
+        <img src={linkedinIcon}></img>
+        </div>
+
+
     </div>
 
 

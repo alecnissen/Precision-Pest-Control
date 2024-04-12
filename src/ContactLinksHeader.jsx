ca5c1e651989a1e29cbd699b45c28e22c7ffdb82
alecnissen <alecnissen@yahoo.com>
2024-04-11 22:48:08 -0400

    added icons that go with the phone, email, and hours content, working on responsiveness now

--- src/ContactLinksHeader.jsx
+++ src/ContactLinksHeader.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import phoneIcon from '../assets/telephone-svgrepo-com.svg';
+import phoneIcon from '../assets/telephone-receiver-material-svgrepo-com.svg';
 import instaIcon from '../assets/instagram-logo-facebook-2-svgrepo-com (1).svg';
 import fbIcon from '../assets/fb-logo.svg';
 import linkedinIcon from '../assets/linkedin-svgrepo-com (1).svg';
+// import phoneIcon from '../assets/telephone-svgrepo-com.svg';
+import emailLogo from "../assets/email-1-svgrepo-com.svg";
+import clockLogo from "../assets/clock-three-svgrepo-com.svg"
+
 
 
 export default function ContactLinksHeader() {
@@ -12,14 +16,17 @@ export default function ContactLinksHeader() {
     <div className='phone-email-hours-container'> 
 
     <div className='phone-container-contact-header'>
+    <img src={phoneIcon}></img>
     <p>267-603-1811</p>
     </div>
 
     <div className='email-container-contact-header'>
+    <img src={emailLogo}></img>
     <p>matt@prodigypestsolutions.com</p>
     </div>
     
     <div className='hours-container-contact-header'>
+    <img src={clockLogo}></img>
     <p>Mon-Fri: 8:00am - 4:00pm</p>
     </div>
 

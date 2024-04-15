73891b4767db794a457ddd845a506bc97562635e
alecnissen <alecnissen@yahoo.com>
2024-04-15 15:02:16 -0400

    added green check marks under title, now will add the call to action button

--- src/ImageTitleComponent.jsx
+++ src/ImageTitleComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ppsLogo from '../assets/PPS-logo.jpeg';
 import backgroundImg from "../assets/hero-2.png";
+import greenCheckMark from "../assets/check-mark-svgrepo-com.svg";
 
 
 
@@ -14,7 +15,26 @@ export default function ImageTitleComponent() {
       {/* <img src={backgroundImg}></img> */}
 
       <div className='title-cta-button-container'>
-      <h1>Effective Pest Control Solutions</h1>
+      <h1>Effective Pest Control <span className='solutions-text-styles'>Solutions</span></h1>
+      </div>
+
+
+      <div className='check-marks-features'>
+          <div className='check-mark-1-pair'>
+          <img src={greenCheckMark}></img>
+          <h3>Organic Treatment</h3>
+          </div>
+
+          <div className='check-mark-2-pair'>
+          <img src={greenCheckMark}></img>
+          <h3>Customer Satisfaction</h3>
+          </div>
+
+          <div className='check-mark-3-pair'>
+          <img src={greenCheckMark}></img>
+          <h3>Licensed and Insured</h3>
+          </div>
+
       </div>
 
     </div>

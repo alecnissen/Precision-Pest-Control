4f6f9b07bee7b29f9039e6ce7b2e5531abd42473
alecnissen <alecnissen@yahoo.com>
2024-04-20 12:53:24 -0400

    changed the size of the images within the navbar, also made the navbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <>
-    <ContactLinksHeader></ContactLinksHeader>
+    {/* <ContactLinksHeader></ContactLinksHeader> */}
     <Navbar></Navbar>
     <ImageTitleComponent></ImageTitleComponent>
     <InsectCards></InsectCards>

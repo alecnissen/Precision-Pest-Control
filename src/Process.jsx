791bffc759f9ee95ffeb3bae4c3fe3de1179c4c8
alecnissen <alecnissen@yahoo.com>
2024-04-24 14:57:07 -0400

    added the last two process steps cards, will begin on next section

--- src/Process.jsx
+++ src/Process.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import msgImg from "../assets/messages-1-svgrepo-com.svg";
-import calenderImg from "../assets/calender-svgrepo-com.svg"
-
-
-
+import msgImg from '../assets/messages-1-svgrepo-com.svg';
+import calenderImg from '../assets/calender-svgrepo-com.svg';
+import dollarBillImg from '../assets/three-dollar-banknotes-svgrepo-com.svg';
+import handshakeImg from '../assets/handshake-svgrepo-com.svg';
 
 export default function Process() {
   return (
@@ -23,80 +22,61 @@ export default function Process() {
 
       <div className="process-cards-container">
         <div className="process-card">
+          <div className="process-step-number-container">
+            <h4>01</h4>
+          </div>
 
-
-           <div className='process-step-number-container'> 
-           <h4>01</h4>
-           </div>
-
-          <div className='process-card-img-container'> 
-          <img src={msgImg}></img>
+          <div className="process-card-img-container">
+            <img src={msgImg}></img>
           </div>
 
           <h3>Project Discussion</h3>
 
           <p>Discuss your pest control needs with our experts to determine the best solution</p>
         </div>
-      
-      
-      
+
         <div className="process-card">
+          <div className="process-step-number-container">
+            <h4>02</h4>
+          </div>
 
-
-
-        <div className='process-step-number-container'> 
-           <h4>02</h4>
-        </div>
-
-
-        <div className='process-card-img-container'> 
-          <img src={calenderImg}></img>
+          <div className="process-card-img-container">
+            <img src={calenderImg}></img>
           </div>
 
           <h3>Schedule Online</h3>
 
           <p>Book an appointment online at your convenience for our team to visit your property</p>
-        </div> 
-
-        <div className="process-card">
-          <h3>Project Discussion</h3>
-
-          <p>Discuss your pest control needs with our experts to determine the best solution</p>
         </div>
 
         <div className="process-card">
-          <h3>Project Discussion</h3>
+          <div className="process-step-number-container">
+            <h4>03</h4>
+          </div>
 
-          <p>Discuss your pest control needs with our experts to determine the best solution</p>
+          <div className="process-card-img-container">
+            <img src={dollarBillImg}></img>
+          </div>
+
+          <h3>Pay Online Easily</h3>
+
+          <p>Make secure online payment for our services, ensuring a hassle-free transaction.</p>
         </div>
-      
-      
-      
-      
-      
-      
-      
-      
-      
+
+        <div className="process-card">
+          <div className="process-step-number-container">
+            <h4>04</h4>
+          </div>
+
+          <div className="process-card-img-container">
+            <img src={handshakeImg}></img>
+          </div>
+
+          <h3>Project Finish</h3>
+
+          <p>Our experts ensure efficient, satisfactory completion of your pest control project</p>
+        </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     </div>
   );
 }

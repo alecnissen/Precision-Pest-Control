117e5cfaaf3cceadf8e2eefdec902a147e9a895d
alecnissen <alecnissen@yahoo.com>
2024-04-24 12:42:22 -0400

    added the features cards within the features section. Now saving work, I will replace the trusted solution pictures with stock photos for now, then begin on the next section

--- src/FeaturesHomepage.jsx
+++ src/FeaturesHomepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import phoneIcon from "../assets/check-mark-circle-svgrepo-com.svg";
+import employeeImg from "../assets/person-disinfecting-restricted-area.jpg";
 
 export default function FeaturesHomepage() {
   return (
@@ -16,6 +17,13 @@ export default function FeaturesHomepage() {
     </div>
 
 
+   <div className='container-for-features-cards'> 
+
+   
+
+    <div className='features-card-set-1'> 
+
+
     <div className='features-card'> 
 
       <img src={phoneIcon} className='features-phone-icon'></img>
@@ -28,14 +36,115 @@ export default function FeaturesHomepage() {
          
     </div>
 
+
+
+
+    <div className='features-card'> 
+
+<img src={phoneIcon} className='features-phone-icon'></img>
+
+
+
+  <h3>Lasting Results</h3>
+
+  <p>Sustainable impact that extends well into the future</p>
+   
+</div>
+
+
+    </div>
+
+
+
+
+    <div className='features-card-set-2'> 
+
+    <div className='features-card'> 
+
+<img src={phoneIcon} className='features-phone-icon'></img>
+
+
+
+  <h3>Safe Methods</h3>
+
+  <p>Protecting your health and the environment with safety</p>
+   
+</div>
+
+<div className='features-card'> 
+
+<img src={phoneIcon} className='features-phone-icon'></img>
+
+
+
+  <h3>Proactive Prevention</h3>
+
+  <p>Prevent pests before they become a problem</p>
+   
+</div>
+
+
+
     </div>
 
 
 
 
 
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+    <div className='numbers-container'> 
+
+
+    <div className='experience-container'> 
+        <h1>35+</h1>
+        <p>Years Experience</p>
+    </div>
+
+    <div className='volunteers-container'> 
+        <h1>800+</h1>
+        <p>Great Volunteers</p>
+        
+    </div>
+
+
+
+    <div className='satisfied-clients-container'> 
+        <h1>35K</h1>
+        <p>Satisfied Clients</p>
+
+    </div>
+
+
+
+
+    </div>
+
+
+
+
+    </div>
+
+
+
+    <div className='features-right-side-container'> 
+        <img src={employeeImg} className='features-employee-img'></img>
+    </div>
     
     
     </div>
   )
 }
+
+
+

5bcf324e78670db84e12c241a4fc9b93727fd824
alecnissen <alecnissen@yahoo.com>
2024-04-30 13:33:00 -0400

    fixed some of the default styles from bootstrap, working on the testimonial section now

--- src/TestimonialsHomepage.jsx
+++ src/TestimonialsHomepage.jsx
@@ -1,3 +1,8 @@
+// import React from 'react';
+// import Carousel from 'react-bootstrap/Carousel';
+// import slide1 from "../assets/carousel-slide-1.png"
+
+
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import slide1 from "../assets/carousel-slide-1.png"
@@ -10,32 +15,20 @@ export default function TestimonialsHomepage() {
 
     <div className='testimonial-content-card-container'> 
 
-    <Carousel>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-          <img src={slide1}></img>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Second slide" /> */}
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Carousel id='event-carousel-container'>
+        <Carousel.Item>
+            <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+        </Carousel.Item>
+      </Carousel>
 
     </div>
 
@@ -43,3 +36,79 @@ export default function TestimonialsHomepage() {
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function TestimonialsHomepage() {
+//   return (
+//     <div className='testimonials-content-wrapper'>
+      
+
+//     <div className='testimonial-content-card-container'> 
+
+//     <Carousel>
+//       <Carousel.Item>
+//         {/* <ExampleCarouselImage text="First slide" /> */}
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//           <img src={slide1}></img>
+          
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         {/* <ExampleCarouselImage text="Second slide" /> */}
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         {/* <ExampleCarouselImage text="Third slide" /> */}
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+
+//     </div>
+
+
+//     </div>
+//   )
+// }

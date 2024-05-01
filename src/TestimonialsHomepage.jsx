132288ac91e1b55a88a17e23caf3a2f7ed2193cc
alecnissen <alecnissen@yahoo.com>
2024-05-01 16:18:06 -0400

    added testimonial cards but now need to figure out why the container is shifting slightly when the slides change

--- src/TestimonialsHomepage.jsx
+++ src/TestimonialsHomepage.jsx
@@ -5,37 +5,108 @@
 
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import slide1 from "../assets/carousel-slide-1.png"
+import slide1 from "../assets/new-testimonal-img-1.png";
+import slide2 from "../assets/slide-2-update.png"
+import slide3 from "../assets/slide-3-img.png";
+import quoteImg from '../assets/quote-svgrepo-com.svg';
+import slide4 from "../assets/slide-4.png"
+
 
 
 export default function TestimonialsHomepage() {
   return (
-    <div className='testimonials-content-wrapper'>
-      
+    <div className='testimonial-content-wrapper'>
+        
+        <div className='testimonial-content-card-container'> 
 
-    <div className='testimonial-content-card-container'> 
+            <div className='testimonial-img-container'> 
 
-    <Carousel id='event-carousel-container'>
-        <Carousel.Item>
+                <img src={quoteImg}></img>
+
+                <div className='testimonial-header-container'> 
+
+                <h3>See Why Clients Love Our Pest Control Service</h3>
+
+                <h1>Hear from <span className='those-who-trust-text'>Those Who Trust</span> Our Pest Control Expertise</h1>
+
+                </div>
+
+
+            </div>
+
+
+
+        <Carousel id='event-carousel-container'>
+         <Carousel.Item>
             <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
         </Carousel.Item>
         <Carousel.Item>
-        <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+        <img id='carousel-image' className="img-fluid" src={slide2} alt='testimonial'></img>
         </Carousel.Item>
         <Carousel.Item>
-        <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
-        </Carousel.Item>
-        <Carousel.Item>
-        <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+         <img id='carousel-image' className="img-fluid" src={slide3} alt='testimonial'></img>
+         </Carousel.Item>
+         <Carousel.Item>
+        <img id='carousel-image' className="img-fluid" src={slide4} alt='testimonial'></img>
         </Carousel.Item>
       </Carousel>
 
-    </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
 
 
     </div>
   )
 }
+
+
+
+
+// export default function TestimonialsHomepage() {
+//   return (
+//     <div className='testimonials-content-wrapper'>
+      
+
+//     <div className='testimonial-content-card-container'> 
+
+//     <img src={quoteImg}></img>
+
+
+
+//     <h3>See Why Clients Love Our Pest Control Services</h3>
+
+//     <h1>Hear from <span>Those Who Trust</span> Our Pest Control Expertise</h1>
+
+//     <Carousel id='event-carousel-container'>
+//         <Carousel.Item>
+//             <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+//         </Carousel.Item>
+//         <Carousel.Item>
+//         <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+//         </Carousel.Item>
+//         <Carousel.Item>
+//         <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+//         </Carousel.Item>
+//         <Carousel.Item>
+//         <img id='carousel-image' className="img-fluid" src={slide1} alt='testimonial'></img>
+//         </Carousel.Item>
+//       </Carousel>
+
+//     </div>
+
+
+//     </div>
+//   )
+// }
 
 
 

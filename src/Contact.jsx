913863302da6699d302f51862737bee7c78d1bee
alecnissen <alecnissen@yahoo.com>
2024-05-26 20:33:50 -0400

    added content for contact section page, re-used components from navbar, contact form and footer

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import ContactForm from './ContactForm';
+import Footer from './Footer';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <> 
+    <div className='contact-tab-content-wrapper'> 
+
+    <Navbar></Navbar>
+
+      {/* <br></br> 
+      <br></br> */}
+
+   
+      <div className='wrapper'> 
+    <ContactForm></ContactForm>
     </div>
+
+
+    <Footer></Footer>
+
+
+
+    </div>
+    
+    
+    </>
   )
 }

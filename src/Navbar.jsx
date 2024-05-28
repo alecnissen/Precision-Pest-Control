1f1cf3c46a46518c61dae0a7477488197f075886
alecnissen <alecnissen@yahoo.com>
2024-05-28 13:00:36 -0400

    changed the jsx in navbar services and pest id so they are matching with the same pests, saving work and now will hook up the pest so it scrolls to the right section

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -64,12 +64,10 @@ export default function Navbar() {
           <a href="">Ants</a>
           <a href="">Bed Bugs</a>
           <a href="">Cockroaches</a>
+          <a href="">Rodents</a>
+          <a href="">Mosquitos</a>
           <a href="">Fleas</a>
-          <a href="">Mosquito</a>
           <a href="">Spiders</a>
-          <a href="">Stinging Insects</a>
-          <a href="">Termites</a>
-          <a href="">Ticks</a>
         </div>
       </div>
 
@@ -100,34 +98,16 @@ export default function Navbar() {
         <div className="content">
           <a href="">Ants</a>
           <a href="">Bed Bugs</a>
-          <a href="">Boxelder Bugs</a>
-          <a href="">Carpenter Bees</a>
           <a href="">Cockroaches</a>
-          <a href="">Fleas</a>
-          <a href="">Mice</a>
+          <a href="">Rodents</a>
           <a href="">Mosquitos</a>
-          <a href="">Rats</a>
+          <a href="">Fleas</a>
           <a href="">Spiders</a>
 
-          <div className="dropdown-for-stinging-pests">
-            <div className="stinging-pests-dropdown-icon-container">
-              <a href="">Stinging Pests</a>
-            </div>
-
-        
-          </div>
-
-          <a href="">Termites</a>
-
-          <div className="dropdown-for-other-pests">
-            <div className="other-pests-dropdown-icon-container">
-              <a href="">Other</a>
-            </div>
-
-         
+          
 
 
-          </div>
+
         </div>
       </div>
 
